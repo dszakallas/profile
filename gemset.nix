ee6c2d2e6ecd0dfcbab9b552b932d9cb91a9ff22
Dávid Szakállas <5807322+dszakallas@users.noreply.github.com>
2025-09-20 22:10:52 +0200

    update gemset.nix

--- gemset.nix
+++ gemset.nix
@@ -475,10 +475,10 @@
     platforms = [ ];
     source = {
       remotes = [ "https://rubygems.org" ];
-      sha256 = "1j9p66pmfgxnzp76ksssyfyqqrg7281dyi3xyknl3wwraaw7a66p";
+      sha256 = "05y4lwzci16c2xgckmpxkzq4czgkyaiiqhvrabdgaym3aj2jd10k";
       type = "gem";
     };
-    version = "3.3.9";
+    version = "3.4.2";
   };
   rouge = {
     groups = [ "default" ];
